4f29937c5b39f35df4bdacdc4cadba9af9fb77fc
Preeti Ganagi <you@example.com>
2017-11-29 17:29:40 +0530

    header footer file is added

---
+++ nextPage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h2> successfull registration </h2>
+
+</body>
+</html>
---
+++ tryDemo.php
@@ -0,0 +1,19 @@
+<?php
+echo print("kiran");
+
+
+echo "<br>";
+
+$var1 = 10;
+$var2 = 20;
+
+//print $var1,$var2;
+echo "<br>";
+echo $var1,$var2;
+echo "<br>";
+print_r($var1.$var2);
+
+
+
+
+?>
